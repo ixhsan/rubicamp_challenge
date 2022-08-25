67662b4d18155645838c4af13b9b252ba464dbca
calliast <muharipan@gmail.com>
2022-08-25 21:24:51 +0700

    update c13

--- c13.js
+++ c13.js
@@ -1,69 +1,201 @@
-const { writeFile } = require('node:fs');
-const fs = require('node:fs');
+const { writeFile } = require("node:fs");
+const fs = require("node:fs");
 
-const userInput = process.argv
-const dataPath = './c13_data.json'
-let message = 'no message'
-let updateExec = ''
+const [a, b, userInput, numInput, ...arr] = process.argv;
+const userInputNum = parseInt(numInput);
+const getTime = new Date();
+const timeFormat = `${getTime.getHours()}:${getTime.getMinutes()}`;
+const dataFormat = { task: "sample", status: [" "], time: " " };
+let resultMessage = "no message";
 
-let dataSource = JSON.parse(fs.readFileSync(dataPath, 'utf-8'))
+let dataPath = "./c13_data.json";
+let dataSource = JSON.parse(fs.readFileSync(dataPath, "utf-8"));
 
-const dataFormat = { task: "sample", status: "not yet" }
-const [a, b, c, ...arr] = userInput
+// FUNGSI WRITE FILE
+const updateData = (message) => {
+  fs.promises
+    .writeFile(dataPath, JSON.stringify(dataSource), "utf-8")
+    .then(() => console.log(`${message}`))
+    .catch(console.error());
+};
 
-// TEST CODE
-// dataFormat.task = arr.join(' ')
-// dataSource.push(dataFormat)
+// CHECK ARGUMENT
+if (userInput.length <= 2) {
+  console.log(`>>> JS TODO <<<
+node todo.js <userInput>
+node todo.js list 
+node todo.js task <task_id>
+node todo.js add <task_content>
+node todo.js delete <task_id>
+node todo.js complete <task_id>
+node todo.js uncomplete <task_id>
+node todo.js list:outstanding <task_id>
+node todo.js list:completed asc||desc
+node todo.js tag <task_id> <tag_name_1> <tag_name_2> ... <tag_name_N>
+node todo.js complete <task_id>
+node todo.js filter:<tagname>\n`);
+} else {
+  // LIST Command
+  if (userInput === "list") {
+    let index = 1;
+    for (let i in dataSource) {
+      console.log(`${index}. [${dataSource[i].status}] ${dataSource[i].task}`);
+      index++;
+    }
+  }
+  // TASK Command
+  else if (userInput === "task") {
+    console.log(`this is <TASK> userInput`);
+  }
+  // ADD Command
+  else if (userInput === "add") {
+    //Tambah tugas baru
+    dataFormat.task = arr.join(" ");
+    dataSource.push(dataFormat);
 
-// console.log(dataSource.length);
-// console.log(dataSource);
+    //Tambah waktu penambahan
+    dataFormat.time = timeFormat;
+    resultMessage = `"${dataFormat.task}" telah ditambahkan.`;
 
-// fs.promises.writeFile(dataPath, JSON.stringify(dataSource), 'utf-8')
-//     .then(() => console.log(`${message}`))
-//     .catch(console.error())
+    // Test output
+    updateData(resultMessage);
+  }
+  // DELETE Command
+  else if (userInput === "delete") {
+    if (userInputNum <= dataSource.length) {
+      // let index = parseInt(userInput[3]) - 1;
+      resultMessage = `"${dataSource[userInputNum - 1].task
+        }" telah dihapus dari daftar`;
+      dataSource.splice(userInputNum - 1, 1);
+      // TEST OUTPUT
+      updateData(resultMessage);
+    } else {
+      console.log(
+        `Operasi gagal!, sepertinya kamu mencari data yang tidak ada.`
+      );
+    }
+  }
+  // COMPLETE Command
+  else if (userInput === "complete") {
+    if (
+      userInputNum <= dataSource.length &&
+      dataSource[userInputNum - 1].status.toString() === " "
+    ) {
+      // Driver code
+      dataSource[userInputNum - 1].status.splice(0, 1, "X");
+      resultMessage = `"${dataSource[userInputNum - 1].task}" telah selesai.`;
+      // Data update
+      updateData(resultMessage);
+    } else {
+      console.log(
+        `Operasi gagal!, sepertinya kamu mencari data yang tidak ada,\natau statusnya telah selesai.`
+      );
+    }
+  }
+  // UNCOMPLETED Command
+  else if (userInput === "uncomplete") {
+    if (userInputNum <= dataSource.length && dataSource[userInputNum - 1].status.toString() === "X"
+    ) {
+      // Driver code
+      dataSource[userInputNum - 1].status.splice(0, 1, " ");
+      resultMessage = `"${dataSource[userInputNum - 1].task
+        }" status selesai dibatalkan.`;
+      // Data update
+      updateData(resultMessage);
+    } else {
+      console.log(`Operasi gagal!, sepertinya kamu mencari data yang tidak ada,\natau statusnya telah selesai.`);
+    }
+  }
+  // TAG Command
+  else if (userInput === "tag") {
+    if (userInputNum <= dataSource.length) {
+      //Driver Code
+      for (let i in arr) {
+        dataSource[userInputNum - 1].tag.push(arr[i]);
+      }
 
-// DRIVER CODE
-const updateData = (data, message) => {
+      resultMessage = `Tag '${arr.toString()}' telah ditambahkan ke daftar '${dataSource[userInputNum - 1].task}'`;
 
-    dataSource.push(data)
+      updateData(resultMessage);
+    } else {
+      console.log(`Operasi gagal!, sepertinya kamu mencari data yang tidak ada.`);
+    }
+  }
+  // FILTER Command
+  else if (userInput === "filter") {
+    console.log(`berhasil ${userInput}`);
+  }
+  // LIST OUTSTANDING Command
+  else if (userInput === "list:outstanding") {
+    if (arr[0] === "asc") {
+      // Input ASC
 
-    fs.promises.writeFile(dataPath, JSON.stringify(data), 'utf-8')
-        .then(() => console.log(`${message}`))
-        .catch(console.error())
+      dataSource.sort((a, z) => {
+        return a.time.localeCompare(z.time);
+      });
 
+      let index = 1;
+      for (let i in dataSource) {
+        if (dataSource[i].status.toString() === " ") {
+          console.log(`${index}. [${dataSource[i].status}] ${dataSource[i].task}\t\t\t\t\t\t${dataSource[i].time}`);
+          index++;
+        }
+      }
+    } else if (arr[0] === "desc") {
+      // Input DESC
+
+      dataSource.sort((a, z) => {
+        return z.time.localeCompare(a.time);
+      });
+
+      let index = 1;
+      for (let i in dataSource) {
+        if (dataSource[i].status.toString() === " ") {
+          console.log(`${index}. [${dataSource[i].status}] ${dataSource[i].task}\t\t\t\t\t\t${dataSource[i].time}`);
+          index++;
+        }
+      }
+    } else {
+      // Error Handling
+      console.log(`Operasi gagal! Silahkan masukan opsi pengurutan: asc | desc`);
+    }
+  }
+  // LIST COMPLETED Command
+  else if (userInput === "list:completed") {
+    if (arr[0] === "asc") {
+      // Input ASC
+
+      dataSource.sort((a, z) => {
+        return a.time.localeCompare(z.time);
+      });
+
+      let index = 1;
+      for (let i in dataSource) {
+        if (dataSource[i].status.toString() === "X") {
+          console.log(`${index}. [${dataSource[i].status}] ${dataSource[i].task}\t\t\t\t\t\t${dataSource[i].time}`);
+          index++;
+        }
+      }
+    } else if (arr[0] === "desc") {
+      // Input DESC
+
+      dataSource.sort((a, z) => {
+        return z.time.localeCompare(a.time);
+      });
+
+      let index = 1;
+      for (let i in dataSource) {
+        if (dataSource[i].status.toString() === "X") {
+          console.log(`${index}. [${dataSource[i].status}] ${dataSource[i].task}\t\t\t\t\t\t${dataSource[i].time}`);
+          index++;
+        }
+      }
+    } else {
+      // Error Handling
+      console.log(`Operasi gagal! Silahkan masukan opsi pengurutan: asc | desc`);
+    }
+  }
+  // TEST Command
+  else if (userInput === "test") {
+  }
 }
-
-if (userInput[2] === 'add') {
-
-    dataFormat.task = arr.join(' ')
-    message = `${dataFormat.task} telah ditambahkan.`
-
-    // // TEST OUTPUT
-    // updateData(dataFormat, message)
-
-}
-
-// --------------- EXPERIMENT ---------------
-// switch (userInput.length) {
-//     case 2:
-//         console.log(`>>> JS TODO <<<
-// node todo.js <command>
-// node todo.js list 
-// node todo.js task <task_id>
-// node todo.js add <task_content>
-// node todo.js delete <task_id>
-// node todo.js complete <task_id>
-// node todo.js uncomplete <task_id>
-// node todo.js list:outstanding <task_id>
-// node todo.js list:completed asc||desc
-// node todo.js tag <task_id> <tag_name_1> <tag_name_2> ... <tag_name_N>
-// node todo.js complete <task_id>
-// node todo.js filter:<tagname>\n`)
-//         break
-//     case 3:
-//         console.log(`ada`)
-//         break
-//     case 4:
-//         console.log(`haha`)
-//         break
-// }
--- c14.js
+++ c14.js
@@ -1,258 +1,60 @@
-// // var str = [{"UserName":"xxx","Rolename":"yyy"}]; // your response in a string
-// // var parses = str.toString(); // an *array* that contains the user
-// // console.log(parses);
-// // var parsed = JSON.parse(str); // an *array* that contains the user
-// // var user = parsed[0];         // a simple user
-// // console.log(user.UserName);   // you'll get xxx
-// // console.log(user.Rolename);   // you'll get yyy
+const { time } = require("node:console");
+const fs = require("node:fs");
 
-// // if data is not in string format
-// const data = [{"UserName":"xxx","Rolename":"yyy"}];
+// let array = []
+// array.push(' ')
+// console.log(array.length);
+// console.log(array);
+// array.splice(0, 1, 'X')
+// console.log(array);
+// console.log(dataSource[index].status.toString() === "X");
 
-// const username = data[0].UserName
-// const rolename = data[0].Rolename
+//TEST TIME
+let dataSource = JSON.parse(fs.readFileSync("./c13_data.json", "utf-8"));
+let index = 0;
+let array = { task: "sample", status: [" "], time: ' ', date: ' ' }
 
-// console.log(username)
-// console.log(rolename)
+let getTime = new Date();
+let timeFormat = `${getTime.getHours()}:${getTime.getMinutes()}`;
+let dateFormat = `${getTime.getDate()}-${getTime.getMonth()}-${getTime.getFullYear()}`;
 
-// // if data is in string format
-// const strData = JSON.parse('[{"UserName":"xxx","Rolename":"yyy"}]');
+console.log(`\nini time di array '${array.time}'`);
+console.log(`ini timeFormat '${timeFormat}'`);
+array.time = timeFormat
+console.log(`ini time di array baru '${array.time}'\n`);
 
-// const Username = strData[0].UserName
-// const Rolename = strData[0].Rolename
+console.log(`ini date di array '${array.date}'`);
+console.log(`ini dateFormat ${dateFormat}`);
+array.date = dateFormat
+console.log(`ini date di array baru '${array.date}'\n`);
 
-// console.log(Username)
-// console.log(Rolename)
+//TEST DATA
 
-const args = process.argv;
-const fs = require('fs');
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-})
+var data = [
+  { transportnumber: "45", time: "10:28:00", date: "2017-01-16" },
+  { transportnumber: "45", time: "10:38:00", date: "2017-01-16" },
+  { transportnumber: "45", time: "10:48:00", date: "2017-01-16" },
+  { transportnumber: "14", time: "10:12:00", date: "2017-01-16" },
+  { transportnumber: "14", time: "10:24:00", date: "2017-01-16" },
+  { transportnumber: "14", time: "10:52:00", date: "2017-01-16" },
+  { transportnumber: "14", time: "10:54:00", date: "2017-01-20" },
+  { transportnumber: "14", time: "08:33:00", date: "2017-01-19" },
+];
 
-let data = JSON.parse(fs.readFileSync('./data-todo.json', 'utf-8'))
+data.sort(function (a, b) {
+  return a.time.localeCompare(b.time);
+});
 
-var DiString;
-var Ngkeheula = 100
-const Boboko = []
-var Langseng = { task: "test", status: " ", time: "test" }
+console.log(data);
 
-function Kenek() {
-    console.log(Object.keys(data).length)
+// TEST MODIF FUNCTION
+var obj = '{ firstName:"John", lastName:"Doe" }';
 
+var jsonStr = obj.replace(/(\w+:)|(\w+ :)/g, function(matchedStr) {
+  return '"' + matchedStr.substring(0, matchedStr.length - 1) + '":';
+});
 
-}
+obj = JSON.parse(jsonStr); //converts to a regular object
 
-if (args.length < 3) {
-    console.log(`\n>>> JS TODO <<<
-node todo.js <command>
-node todo.js list 
-node todo.js task <task_id>
-node todo.js add <task_content>
-node todo.js delete <task_id>
-node todo.js complete <task_id>
-node todo.js uncomplete <task_id>
-node todo.js list:outstanding <task_id>
-node todo.js list:completed asc||desc
-node todo.js tag <task_id> <tag_name_1> <tag_name_2> ... <tag_name_N>
-node todo.js complete <task_id>
-node todo.js filter:<tagname> \n`)
-
-    setTimeout(function () {
-        process.exit(0)
-    }, Ngkeheula)
-
-} else if (args[2] == 'add') {
-
-    for (let i = 3; i < args.length; i++) {
-        Boboko.push(args[i])
-    }
-
-    DiString = Boboko.join(' ')
-    Langseng.task = DiString
-    var time = new Date()
-    var h = `${time.getHours()}:${time.getMinutes()}`
-    Langseng.time = h
-    data.push(Langseng)
-    console.log(typeof data);
-    
-    fs.writeFile('./data-todo.json', JSON.stringify(data), (err) => {
-        if (err) console.log(err)
-        console.log(`"${DiString}" telah ditambahkan.`)
-    })
-
-
-    setTimeout(function () {
-        process.exit(0)
-    }, Ngkeheula)
-
-} else if (args[2] == 'list') {
-
-    console.log(`Daftar pekerjaan.\n`)
-    let Kenek = 1
-    data.forEach(function (Obj) {
-        console.log(`${Kenek}. [${Obj.status}] ${Obj.task}`)
-        Kenek++
-    })
-
-    setTimeout(function () {
-        process.exit(0)
-    }, Ngkeheula)
-
-} else if (args[2] == 'list:outstanding') {
-
-
-    let Kenek = 1
-    let wadah = []
-    data.forEach(function (Obj) {
-        if (Obj.status == " ") {
-            wadah.push(Obj)
-            Kenek++
-        }
-        return wadah
-    })
-
-    if (wadah.length !== 0) {
-        // console.log(wadah.length)
-        if (args.length < 4) {
-            console.log("tolong masukkan parameter urutan. (asc/desc)")
-
-        } else if (args[3] == "desc") {
-            console.log(`Daftar pekerjaan outstanding.\nsort:desc.\n`)
-            let Kenek = wadah.length
-            wadah.reverse().forEach(function (Obj) {
-                // console.log(Object.keys(Obj)[2])
-                console.log(`${Kenek}. [${Obj.status}] ${Obj.task}`)
-                Kenek--
-            })
-        }
-        else if (args[3] == "asc") {
-            console.log(`Daftar pekerjaan outstanding.\nsort:asc.\n`)
-            wadah.forEach(function (Obj) {
-                console.log(`${Kenek}. [${Obj.status}] ${Obj.task}`)
-                Kenek++
-            })
-        }
-    } else {
-        console.log("Tidak ada tugas outstanding.")
-    }
-
-    setTimeout(function () {
-        process.exit(0)
-    }, Ngkeheula)
-
-} else if (args[2] == 'delete') {
-
-    let order = parseInt(args[3]) - 1
-    data.splice(order, 1)
-
-    fs.writeFile('./data-todo.json', JSON.stringify(data), (err) => {
-        if (err) console.log(err)
-        console.log(`"${data[order].task}" telah dihapus dari daftar`)
-    })
-
-    setTimeout(function () {
-        process.exit(0)
-    }, Ngkeheula)
-
-}
-else if (args[2] == 'complete') {
-
-    let order = parseInt(args[3]) - 1
-    data[order].status = "x"
-
-    fs.writeFile('./data-todo.json', JSON.stringify(data), (err) => {
-        if (err) console.log(err)
-        console.log(`"${data[order].task}" telah selesai.`)
-    })
-
-    setTimeout(function () {
-        process.exit(0)
-    }, Ngkeheula)
-}
-else if (args[2] == 'uncomplete') {
-
-    let order = parseInt(args[3]) - 1
-    data[order].status = " "
-
-    fs.writeFile('./data-todo.json', JSON.stringify(data), (err) => {
-        if (err) console.log(err)
-        console.log(`"${data[order].task}" status selesai dibatalkan.`)
-    })
-
-    setTimeout(function () {
-        process.exit(0)
-    }, Ngkeheula)
-
-}
-else if (args[2] == 'tag') {
-    let kenek = args[3] - 1
-    let stiker = args.length - 1
-    let mangkok = []
-    let a
-
-    for (let i = stiker; i >= 4; i--) {
-        mangkok.push(args[i])
-        // kenek++
-    }
-
-    a = mangkok.join(',')
-    // console.log(a)
-
-    let x = 0
-    for (i = x; i <= parseInt(args[3]); i++) {
-        if (i === parseInt(args[3])) {
-            // console.log('sampe')
-            data[x].tag = mangkok
-            fs.writeFile('./data-todo.json', JSON.stringify(data), (err) => {
-                if (err) console.log(err)
-                console.log(`Tag '${data[x].tag}' telah ditambahkan ke daftar '${data[x].task}'`)
-            })
-        } else {
-            x++
-        }
-    }
-
-    setTimeout(function () {
-        process.exit(0)
-    }, Ngkeheula)
-
-} else if (args[2].includes('filter:')) {
-    let a = args[2].replace("filter:", "")
-    // console.log(`berhasil 1 ${a}`)
-
-    if (a == "") {
-        console.log(`harap masukkan parameter filter!.`)
-
-        setTimeout(function () {
-            process.exit(0)
-        }, Ngkeheula)
-
-    } else {
-        let x = 0
-        let Kenek = 1
-        // let b = data[x].tag.join(',')
-        // console.log(`berhasil 2 ${b}`)
-
-        let wadah = []
-        data.forEach(function (Obj) {
-            if (Obj.tag.includes(a)) {
-                wadah.push(Obj)
-                Kenek++
-            }
-            return wadah
-        })
-
-        console.log(`Daftar Pekerjaan dg tag: ${a}\n`)
-        wadah.forEach(function (Obj) {
-            console.log(`${Kenek}. [${Obj.status}] ${Obj.task}`)
-            Kenek++
-        })
-
-        setTimeout(function () {
-            process.exit(0)
-        }, Ngkeheula)
-    }
-}
+console.log(obj.firstName); // expected output: John
+console.log(obj.lastName); // expected output: Doe
