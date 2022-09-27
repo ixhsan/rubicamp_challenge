ef5b065f589c8d461f3c9853dc0114f5ac7ac87d
calliast <muharipan@gmail.com>
2022-09-27 12:02:09 +0700

    reorganize file and folder

--- experiment/exp-1/exp12-array-promise-string.js
+++ experiment/exp-1/exp12-array-promise-string.js
@@ -1,202 +1,3 @@
-// // /* GANTI STRING DALAM ARRAY */
-// // // var obj = {
-// // //     'property': 'Mahasiswa',
-// // //     'key': 'NIM'
-// // //   }
-
-// // //   var text = "{property} dengan {key}: ? tidak terdaftar"
-
-// // //   console.log(mutliStringReplace(obj,text))
-
-// // //   function mutliStringReplace(object, string) {
-// // //         var val = string
-// // //         var entries = Object.entries(object);
-// // //         console.log(entries);
-// // //         entries.forEach((item)=> {
-// // //             var find = '{' + item[0] + '}'
-// // //             var regExp = new RegExp(find,'g')
-// // //          val = val.replace(regExp, item[1])
-// // //          console.log(item);
-// // //       })
-// // //     return val;
-// // //   }
-
-// // // const defaultPrompt = [
-// // //     `masukkan key: `,
-// // //     `property dengan key: ? tidak terdaftar`,
-// // //     `masukkan key property yang akan dihapus: `,
-// // //     `property dengan key: ? telah dihapus`
-// // // ]
-
-// // // const obj = ['mahasiswa', 'jurusan', 'dosen', 'matakuliah', 'kontrak']
-// // // const keys = ['NIM', 'id', 'NIP', 'Kode', 'id']
-
-// // // let textmod = function (value) {
-// // //     let updatedPrompt = []
-// // //     for (let key in defaultPrompt) {
-// // //         updatedPrompt.push(
-// // //             defaultPrompt[key]
-// // //                 .replace(/key/g, keys[value])
-// // //                 .replace(/property/g, props[value])
-// // //         )
-// // //     }
-// // //     return updatedPrompt
-// // // }
-
-// // // console.log(textmod(1));
-// // // let a = 4
-
-// // /* TEST PROMISE */
-// // // const p = new Promise((resolve, reject) => {
-// // //     if (a === 5) {
-// // //         resolve(a)
-// // //     } else {
-// // //         reject(a)
-// // //     }
-// // // })
-
-// // // p.then((message) => {
-// // //     console.log(`waw ${message}`)
-// // // }).catch((message) => {
-// // //     console.log(`waduh ${message}`);
-// // // })
-
-// // // const Obj = {
-// // //     "name": "tesa",
-// // //     "class": "6a"
-// // // }
-
-// // // let c = Object.keys(Obj).toString()
-// // // console.log(c);
-
-// // /* TEST METODE LOOPING ARRAY */
-// // // let a = arrOfTwo.entries()
-// // // for (let [index, key] of  arrOfTwo.entries()) {
-// // //     for (let key in arrOfTwo[index]) {
-// // //         console.log(`ini let of entries ${index}, ${key}`);
-// // //     }
-// // // }
-
-// // // metode looping buat array
-// // // for (let key of arrOfOne) {
-// // //     console.log(`ini let of ` + key);
-// // // }
-
-// // // for (let key in arrOfOne) {
-// // //     console.log(`ini let in ` + key);
-// // // }
-
-// // // for (let key in arrOfTwo[0]) {
-// // //     console.log(key + ' ' + arrOfTwo[0][key]);
-// // // }
-
-// // // TEST METODE BUAT AKSES DATA TYPE ARRAY DALAM OBJECT PROPERTY
-// // // const query = {
-// // //     mahasiswa: [
-// // //         `SELECT * FROM mahasiswa`,
-// // //         `SELECT * FROM mahasiswa WHERE nim=?`,
-// // //         `INSERT INTO mahasiswa(nim, nama, alamat, jurusan, DoB) VALUES(?,?,?,?,?)`,
-// // //         `DELETE FROM mahasiswa WHERE nim=?`,
-// // //         `masukkan NIM: `,
-// // //         `mahasiswa dengan NIM: ? tidak terdaftar!`,
-// // //         `masukkan NIM mahasiswa yang akan dihapus: `,
-// // //         `mahasiswa dengan NIM: ? telah dihapus!`,
-// // //         ['NIM', 'Nama', 'Alamat', 'Jurusan', 'DoB']
-// // //     ],
-// // //     jurusan: [
-// // //         `SELECT * FROM jurusan`,
-// // //         `SELECT * FROM jurusan WHERE idjurusan=?`,
-// // //         `INSERT INTO jurusan(idjurusan,nama_jurusan) VALUES(?,?)`,
-// // //         `DELETE FROM jurusan WHERE idjurusan=?`,
-// // //         `masukkan id jurusan: `,
-// // //         `jurusan dengan id: ? tidak terdaftar!`,
-// // //         `masukkan id jurusan yang akan dihapus:`,
-// // //         `jurusan dengan id: ? telah dihapus!`,
-// // //         ['ID jurusan', 'Jurusan']
-// // //     ],
-// // //     dosen: [
-// // //         `SELECT * FROM dosen`,
-// // //         `SELECT * FROM dosen WHERE nip=?`,
-// // //         `INSERT INTO dosen(nip, nama, matakuliah) VALUES(?,?,?)`,
-// // //         `DELETE FROM dosen WHERE nip=?`,
-// // //         `masukkan nip: `,
-// // //         `dosen dengan nip: ? tidak terdaftar!`,
-// // //         `masukkan nip dosen yang akan dihapus:`,
-// // //         `dosen dengan nip: ? telah dihapus!`,
-// // //         ['NIP', 'Nama Dosen', 'Mata Kuliah']
-// // //     ],
-// // //     matakuliah: [
-// // //         `SELECT * FROM matakuliah`,
-// // //         `SELECT * FROM jurusan WHERE idjurusan=?`,
-// // //         `INSERT INTO matakuliah(kodematkul, nama, sks) VALUES(?,?,?)`,
-// // //         `DELETE FROM matakuliah WHERE kodematkul=?`,
-// // //         `masukkan kode matakuliah: `,
-// // //         `matakuliah dengan kode: ? tidak terdaftar!`,
-// // //         `masukkan kode matakuliah yang akan dihapus: `,
-// // //         `matakuliah dengan kode: ? telah dihapus!`,
-// // //         ['Kode', 'Nama Matakuliah', 'Jumlah SKS']
-// // //     ],
-// // //     kontrak: [
-// // //         `SELECT * FROM kontrak`,
-// // //         `SELECT * FROM kontrak WHERE idkontrak=?`,
-// // //         `INSERT INTO kontrak(id,mahasiswa,dosen,matakuliah,nilai) VALUES(?,?,?,?,?)`,
-// // //         `DELETE FROM kontrak WHERE id=?`,
-// // //         `masukkan id: `,
-// // //         `kontrak dengan id: ? tidak terdaftar!`,
-// // //         `masukkan id kontrak yang akan dihapus:`,
-// // //         `kontrak dengan id: ? telah dihapus!`,
-// // //         ['ID', 'Mahasiswa', 'Dosen', 'Matakuliah', 'Nilai']
-// // //     ]
-// // // }
-
-// // // function setHeader(keys) {
-// // //     const objSet = ['not found', 'mahasiswa', 'jurusan', 'dosen', 'matakuliah', 'kontrak']
-// // //     const setHead = query[objSet[keys]][8]
-// // //     return setHead
-// // // }
-
-// // // const finalArr = setHeader(5)
-
-// // // console.log(finalArr);
-
-// // /* METODE UNTUK AKSES ARRAY OF OBJECT */
-// // // const arrOfTwo = [{
-// // //     nim: '005',
-// // //     nama: 'deri',
-// // //     alamat: 'cianjur',
-// // //     jurusan: 'teknik mesin',
-// // //     DoB: '2003-02-01'
-// // // },
-// // // {
-// // //     nim: '004',
-// // //     nama: 'dani',
-// // //     alamat: 'surabaya',
-// // //     jurusan: 'teknik pangan',
-// // //     DoB: '1999-12-29'
-// // // }]
-
-// // // let arrSelect = []
-// // // let obj = []
-
-// // // for (let i = 0; i < arrOfTwo.length; i++) {
-// // //     for (let key in arrOfTwo[i]) {
-// // //         arrSelect.push(key)
-// // //         console.log(`ini ${key} for ${arrOfTwo[i][key]}`);
-// // //     }
-// // //     obj.push(arrSelect)
-// // //     arrSelect = []
-// // // }
-// // // console.log(obj);
-
-// // /* TEST TERNARY OPERATOR */
-
-// // // let a = 5
-// // // let b = 'test'
-
-// // // typeof a === 'number' ? console.log('number') : console.log('bukan number');
-
-// // // console.log(typeof a);
-
 // // /* TEST MENU SWITCHING DENGAN CASE */
 // // // let b = Obj['dosen']
 // // // let c = Obj['jurusan'] ?? Obj['mahasiswa']
@@ -291,7 +92,6 @@
 
 // // // })
 // // /* activating readline */
-
 
 // // /* TEST LOGIN  */
 // // // function generateObj(value) {
@@ -521,12 +321,6 @@
 // // // }
 // // // rl.prompt()
 
-// // /* TEST NULLISH COALLESCING OPERATOR (??) */
-// // // let Obj = {
-// // //     mahasiswa: 'test',
-// // //     dosen: 'haha'
-// // // }
-
 // // /* SELECTIVE OPERATION */
 
 // // // function pageOperation(input, output, checkOutput = 'no') {
@@ -607,7 +401,7 @@
 
 // import readline from 'node:readline'
 
-// /* DEKLARASI FUNGSI & COMPONENT*/
+/* DEKLARASI FUNGSI & COMPONENT*/
 
 // const rl = readline.createInterface({
 //     input: process.stdin,
@@ -777,6 +571,8 @@
 
 // pageLogin(/*null*/)
 
+/*/////////////////////////////////////////////////////////*/
+
 // /*TEST ARRAY OF FUNCTIONS */
 
 // // function test1() {
@@ -796,6 +592,211 @@
 // // ]
 
 // // testArr[0]()
+
+/* TEST NULLISH COALLESCING OPERATOR (??) */
+// // // let Obj = {
+// // //     mahasiswa: 'test',
+// // //     dosen: 'haha'
+// // // }
+
+/* GANTI STRING DALAM ARRAY */
+// // // var obj = {
+// // //     'property': 'Mahasiswa',
+// // //     'key': 'NIM'
+// // //   }
+
+// // //   var text = "{property} dengan {key}: ? tidak terdaftar"
+
+// // //   console.log(mutliStringReplace(obj,text))
+
+// // //   function mutliStringReplace(object, string) {
+// // //         var val = string
+// // //         var entries = Object.entries(object);
+// // //         console.log(entries);
+// // //         entries.forEach((item)=> {
+// // //             var find = '{' + item[0] + '}'
+// // //             var regExp = new RegExp(find,'g')
+// // //          val = val.replace(regExp, item[1])
+// // //          console.log(item);
+// // //       })
+// // //     return val;
+// // //   }
+
+// // // const defaultPrompt = [
+// // //     `masukkan key: `,
+// // //     `property dengan key: ? tidak terdaftar`,
+// // //     `masukkan key property yang akan dihapus: `,
+// // //     `property dengan key: ? telah dihapus`
+// // // ]
+
+// // // const obj = ['mahasiswa', 'jurusan', 'dosen', 'matakuliah', 'kontrak']
+// // // const keys = ['NIM', 'id', 'NIP', 'Kode', 'id']
+
+// // // let textmod = function (value) {
+// // //     let updatedPrompt = []
+// // //     for (let key in defaultPrompt) {
+// // //         updatedPrompt.push(
+// // //             defaultPrompt[key]
+// // //                 .replace(/key/g, keys[value])
+// // //                 .replace(/property/g, props[value])
+// // //         )
+// // //     }
+// // //     return updatedPrompt
+// // // }
+
+// // // console.log(textmod(1));
+// // // let a = 4
+
+/* TEST PROMISE */
+// // // const p = new Promise((resolve, reject) => {
+// // //     if (a === 5) {
+// // //         resolve(a)
+// // //     } else {
+// // //         reject(a)
+// // //     }
+// // // })
+
+// // // p.then((message) => {
+// // //     console.log(`waw ${message}`)
+// // // }).catch((message) => {
+// // //     console.log(`waduh ${message}`);
+// // // })
+
+// // // const Obj = {
+// // //     "name": "tesa",
+// // //     "class": "6a"
+// // // }
+
+// // // let c = Object.keys(Obj).toString()
+// // // console.log(c);
+
+/* TEST METODE LOOPING ARRAY */
+// // // let a = arrOfTwo.entries()
+// // // for (let [index, key] of  arrOfTwo.entries()) {
+// // //     for (let key in arrOfTwo[index]) {
+// // //         console.log(`ini let of entries ${index}, ${key}`);
+// // //     }
+// // // }
+
+// metode looping buat array
+// // // for (let key of arrOfOne) {
+// // //     console.log(`ini let of ` + key);
+// // // }
+
+// // // for (let key in arrOfOne) {
+// // //     console.log(`ini let in ` + key);
+// // // }
+
+// // // for (let key in arrOfTwo[0]) {
+// // //     console.log(key + ' ' + arrOfTwo[0][key]);
+// // // }
+
+/*TEST METODE BUAT AKSES ARRAY DALAM OBJECT PROPERTY*/
+// // // const query = {
+// // //     mahasiswa: [
+// // //         `SELECT * FROM mahasiswa`,
+// // //         `SELECT * FROM mahasiswa WHERE nim=?`,
+// // //         `INSERT INTO mahasiswa(nim, nama, alamat, jurusan, DoB) VALUES(?,?,?,?,?)`,
+// // //         `DELETE FROM mahasiswa WHERE nim=?`,
+// // //         `masukkan NIM: `,
+// // //         `mahasiswa dengan NIM: ? tidak terdaftar!`,
+// // //         `masukkan NIM mahasiswa yang akan dihapus: `,
+// // //         `mahasiswa dengan NIM: ? telah dihapus!`,
+// // //         ['NIM', 'Nama', 'Alamat', 'Jurusan', 'DoB']
+// // //     ],
+// // //     jurusan: [
+// // //         `SELECT * FROM jurusan`,
+// // //         `SELECT * FROM jurusan WHERE idjurusan=?`,
+// // //         `INSERT INTO jurusan(idjurusan,nama_jurusan) VALUES(?,?)`,
+// // //         `DELETE FROM jurusan WHERE idjurusan=?`,
+// // //         `masukkan id jurusan: `,
+// // //         `jurusan dengan id: ? tidak terdaftar!`,
+// // //         `masukkan id jurusan yang akan dihapus:`,
+// // //         `jurusan dengan id: ? telah dihapus!`,
+// // //         ['ID jurusan', 'Jurusan']
+// // //     ],
+// // //     dosen: [
+// // //         `SELECT * FROM dosen`,
+// // //         `SELECT * FROM dosen WHERE nip=?`,
+// // //         `INSERT INTO dosen(nip, nama, matakuliah) VALUES(?,?,?)`,
+// // //         `DELETE FROM dosen WHERE nip=?`,
+// // //         `masukkan nip: `,
+// // //         `dosen dengan nip: ? tidak terdaftar!`,
+// // //         `masukkan nip dosen yang akan dihapus:`,
+// // //         `dosen dengan nip: ? telah dihapus!`,
+// // //         ['NIP', 'Nama Dosen', 'Mata Kuliah']
+// // //     ],
+// // //     matakuliah: [
+// // //         `SELECT * FROM matakuliah`,
+// // //         `SELECT * FROM jurusan WHERE idjurusan=?`,
+// // //         `INSERT INTO matakuliah(kodematkul, nama, sks) VALUES(?,?,?)`,
+// // //         `DELETE FROM matakuliah WHERE kodematkul=?`,
+// // //         `masukkan kode matakuliah: `,
+// // //         `matakuliah dengan kode: ? tidak terdaftar!`,
+// // //         `masukkan kode matakuliah yang akan dihapus: `,
+// // //         `matakuliah dengan kode: ? telah dihapus!`,
+// // //         ['Kode', 'Nama Matakuliah', 'Jumlah SKS']
+// // //     ],
+// // //     kontrak: [
+// // //         `SELECT * FROM kontrak`,
+// // //         `SELECT * FROM kontrak WHERE idkontrak=?`,
+// // //         `INSERT INTO kontrak(id,mahasiswa,dosen,matakuliah,nilai) VALUES(?,?,?,?,?)`,
+// // //         `DELETE FROM kontrak WHERE id=?`,
+// // //         `masukkan id: `,
+// // //         `kontrak dengan id: ? tidak terdaftar!`,
+// // //         `masukkan id kontrak yang akan dihapus:`,
+// // //         `kontrak dengan id: ? telah dihapus!`,
+// // //         ['ID', 'Mahasiswa', 'Dosen', 'Matakuliah', 'Nilai']
+// // //     ]
+// // // }
+
+// // // function setHeader(keys) {
+// // //     const objSet = ['not found', 'mahasiswa', 'jurusan', 'dosen', 'matakuliah', 'kontrak']
+// // //     const setHead = query[objSet[keys]][8]
+// // //     return setHead
+// // // }
+
+// // // const finalArr = setHeader(5)
+
+// // // console.log(finalArr);
+
+/* METODE UNTUK AKSES ARRAY OF OBJECT */
+// // // const arrOfTwo = [{
+// // //     nim: '005',
+// // //     nama: 'deri',
+// // //     alamat: 'cianjur',
+// // //     jurusan: 'teknik mesin',
+// // //     DoB: '2003-02-01'
+// // // },
+// // // {
+// // //     nim: '004',
+// // //     nama: 'dani',
+// // //     alamat: 'surabaya',
+// // //     jurusan: 'teknik pangan',
+// // //     DoB: '1999-12-29'
+// // // }]
+
+// // // let arrSelect = []
+// // // let obj = []
+
+// // // for (let i = 0; i < arrOfTwo.length; i++) {
+// // //     for (let key in arrOfTwo[i]) {
+// // //         arrSelect.push(key)
+// // //         console.log(`ini ${key} for ${arrOfTwo[i][key]}`);
+// // //     }
+// // //     obj.push(arrSelect)
+// // //     arrSelect = []
+// // // }
+// // // console.log(obj);
+
+/* TEST TERNARY OPERATOR */
+
+// // // let a = 5
+// // // let b = 'test'
+
+// // // typeof a === 'number' ? console.log('number') : console.log('bukan number');
+
+// // // console.log(typeof a);
 
 /* THIS keyword on normal vs arrow functin */
 
@@ -909,11 +910,11 @@
 
 /* TEST TRUTHY DAN FALSY */
 
-let a = 0
-let b = 1
+// let a = 0
+// let b = 1
 
-if (b == true) {
-    console.log(`var is true`);
-} else {
-    console.log(`var is false`);
-}
+// if (b == true) {
+//     console.log(`var is true`);
+// } else {
+//     console.log(`var is false`);
+// }
